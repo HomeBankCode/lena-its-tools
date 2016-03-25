25108398f820d359db3e2fca77d0609622bfe77e
gpretzer <gpretzer@ucmerced.edu>
2016-03-24 21:01:22 -0700

    this version has updated instructions

--- segments.pl
+++ segments.pl
@@ -2,14 +2,17 @@
 #
 # Anne S. Warlaumont
 #
+# This tool allows you to find the start time, end time, and type of vocalization for each vocalization in a LENA recording, using the corresponding .its file.
+# The output information is written into .csv file.
+#
 # Takes the following command line arguments:
-# The (path and) file name of the input its file
-# The (path and) csv file name where you want the segment data to be written
+# The path and file name of the input its file (e.g. "~/lena-its-tools/segments.pl)
+# The path and csv file name where you want the segment data to be written (e.g. "~/lena-its-results/e20131126_155022_009145_segments.csv"
 #
 # Instructions:
 # 1. Open up a unix shell (e.g. the Terminal application under Utilities on Mac or Cygwin on Windows)
 # 2. Navigate to the directory where this file is located (e.g. "~/CodeDirectory/")
-# 3. Run segments.pl with the location and name of the its file as the first argument and the location and name of the desired segments csv output file as the 2nd argument
+# 3. Run segments.pl with the path and file name of the its file as the first argument and the path and name of the desired segments csv output file as the 2nd argument
 # (e.g. "perl segments.pl ~/DataDirectory/e20120417_130404_007941.its ~/DataDirectory/e20120417_130404_007941_segments.csv")
 
 use strict; use warnings;
