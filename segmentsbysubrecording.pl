7bf068accfa30156a2c8559c6caa7526cdabda68
gpretzer <gpretzer@ucmerced.edu>
2016-05-18 17:19:09 -0700

    updated instructions

--- segmentsbysubrecording.pl
+++ segmentsbysubrecording.pl
@@ -1,17 +1,20 @@
 #!/usr/local/bin/perl
 
 # Anne S. Warlaumont
-# Last update: May 16, 2016
+# Last update: May 18, 2016
 
-# This tool identifies segments within subrecordings of a LENA recording.
+# This tool identifies segments within subrecordings of a LENA recording. 
+# A new file containing the start and end times for each segment will be created for each subrecording of the entire recording.
+# e.g., e20131210_144819_009143CHN_AN_Segments1.txt; e20131210_144819_009143CHN_AN_Segments2.txt; e20131210_144819_009143CHN_AN_Segments3.txt
 
 # Take the following command line arguments:
-# 1: The (path and) filename of the pause times file. E.g. "e20080304_150244_003407_PauseTimes.txt"
-# 2: The (path and) filename of the segment times input file. E.g. "e20080304_150244_003407ANStartAndEndTimes_IncludeOverlap.txt"
-# 3: The column of the segment times input file that contains the start time. For single speaker files, this will be the first column. For combined speaker files, this will be the second.
-# e.g. perl segmentsbysubrecording.pl /Users/awarlau/LENAInteraction/MemphisLENARecordings/postitsfiles/e20080828_165836_003407_PauseTimes.txt /Users/awarlau/LENAInteraction/MemphisLENARecordings/postitsfiles/e20080828_165836_003407ANStartAndEndTimes_IncludeOverlap.txt 
+# 1: The (path and) filename of the pause times file. E.g. "e20131210_144819_009143_PauseTimes.txt"
+# 2: The (path and) filename of the segment times input file. E.g. "e20131210_144819_009143CHN_AN_Segments.txt"
+# e.g. perl segmentsbysubrecording.pl ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN_AN_Segments.txt 
 
-use strict; use warnings;
+# For single speaker files, the first column is the start time, but for combined speaker files, the second column is the the start time.
+
+use strict; use warnings;H
 
 print "test segmentsbysubrecording input 1: "; print $ARGV[0]; print "\n";
 print "test segmentsbysubrecording input 2: "; print $ARGV[1]; print "\n";
