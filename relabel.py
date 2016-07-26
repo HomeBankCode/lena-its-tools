6bb7d73246a6e6d5ffd8dd61bfb7ccad3d4f773e
AnneSWarlaumont <anne.warlaumont@gmail.com>
2016-07-26 13:02:04 -0700

    Changed the inputs a bit, tweaked the instructions, and made it so that data are saved. Still need to implement restarting where the user left off.

--- relabel.py
+++ relabel.py
@@ -9,7 +9,8 @@ def relabel_by_timebin(wavfile,outfile,coding_start_time,coding_end_time,bin_siz
     
     sr, sounddata = scipy.io.wavfile.read(wavfile)
     outf = open(outfile,'w')
-
+    outf.write('startSeconds,endSeconds,userInput,containsTargetChild,containsOtherChild,containsAdult\n')
+    
     for playstart in numpy.arange(coding_start_time,coding_end_time,bin_size):
         if playstart+bin_size > coding_end_time:
             playend = coding_end_time
@@ -18,10 +19,15 @@ def relabel_by_timebin(wavfile,outfile,coding_start_time,coding_end_time,bin_siz
         segsounddata = sounddata[playstart*sr:playend*sr]
         scipy.io.wavfile.write('temp.wav',sr,segsounddata)
         pygsound = pyglet.media.load('temp.wav',streaming=False)
-        raw_input("Press Enter to play the sound")
+        raw_input("\nPress Enter to play the sound. Listen carefully because you will only get one opportunity to listen.")
         pygsound.play()
-        raw_input("Please enter the codes (no spaces between) for the types of voices you heard in the segment that just played. Codes: c for child wearing the recorder, x for another child, a for adult. Press enter when you are done entering the codes, then the next sound snippet will automatically play. Press r to replay the segment. Press b to return to the previous segment.")
-    
+        userInput = raw_input("\nPress control+c to quit the labeling session.\n\n***Instructions***\nPlease enter the codes (no spaces between) for the types of voices you heard in the segment that just played.\nCodes: t for child wearing the recorder, o for another child, a for adult.\nIf there are multiple voices present, enter the multiple codes without spaces in between.\nOrder of the codes does not matter.\nLeave the codes blank if you didn't hear any voices. \n\nType the codes for the voices you heard here, then press return: ")
+        containsTargetChild = "t" in userInput
+        containsOtherChild = "o" in userInput
+        containsAdult = "a" in userInput
+        outf.write(str(playstart) + ',' + str(playend) + ',' + userInput + ',' + str(containsTargetChild) + ',' + str(containsOtherChild) + ',' + str(containsAdult) + '\n')
+        # To do: Make it so that if outfile already exists, it picks up the coding from where the listener left off.
+        
     outf.close()
     
     return
