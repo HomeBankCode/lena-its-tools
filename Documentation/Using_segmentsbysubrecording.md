4b1bcd2981829b9e96452e725918582cbc38015c
AnneSWarlaumont <anne.warlaumont@gmail.com>
2016-10-14 09:39:29 -0700

    Modified segmentsbysubrecording.pl to make it possible to run on the output of segments.pl. Running this function will now require specifying two input arguments. See the comments and/or documentation for details.

--- Documentation/Using_segmentsbysubrecording.md
+++ Documentation/Using_segmentsbysubrecording.md
@@ -8,7 +8,7 @@ A new file containing the start and end times for each segment will be created f
 ![alt text](https://github.com/gpretzer/DocumentationPics/blob/master/SegBySub_Pic1.jpg "Title")
 
 Before running this program, you must run:
-* readits\_start\_end\_content.pl
+* readits\_start\_end\_content.pl or segments.pl
 * recorderpauses.pl
 
 How to use "segmentsbysubrecording.pl" on a single .its file (on a Mac):
@@ -20,13 +20,17 @@ How to use "segmentsbysubrecording.pl" on a single .its file (on a Mac):
      
 ![alt text](https://github.com/gpretzer/DocumentationPics/blob/master/SegBySub_Pic2.jpg "Title")
 
-* Run “segmentsbysubrecording.pl” with the location and name of the its file as the first argument and the location and name of the desired segments csv output file as the second argument.
+* Run “segmentsbysubrecording.pl” with the location and name of the its file as the first argument, the location and name of the desired segments csv output file as the second argument, and the numbers 1 and 0 or 2 and 1 as the second and third argument, depending on whether you are running this on the output of readits_start_end_content.pl or segments.pl.
   * In the command line, type “perl segmentsbysubrecording.pl” which calls the script and readies it to run.
   * Next, add the first argument, which is the name of the pause times file made from running “recorderpauses.pl”.
     * e.g. “~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt”
   * Continue by typing in the second argument, which is the path and name of the segment times input file created by "readits\_start\_end\_content.pl".
     * e.g. “~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN\_AN\_Segments.txt”
+   * Next, if you are running this on the output of readits_start_end_content.pl, add "1" and "0" as the third and fourth arguments. Or if you are running it on the output of segments.pl, add "2" and "1".
   * Press Return to run the entire line.
-    * e.g. “perl segmentsbysubrecording.pl ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN\_AN\_Segments.txt
+    * e.g. “perl segmentsbysubrecording.pl ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN\_AN\_Segments.txt 1 0"
+    * OR
+    * "perl segmentsbysubrecording /PathToPauseTimesFile/e20131126_15502_009145_PauseTimes.txt /PathToOutputOfSegmentsDotPl/e20131126_15502_009145_Segments.txt 2 1"
+    * Note that the numbers as 3rd and 4th input arguments are new, and aren't shown in the screenshot below.
     
 ![alt text](https://github.com/gpretzer/DocumentationPics/blob/master/SegBySub_Pic3.jpg "Title")
--- segmentsbysubrecording.pl
+++ segmentsbysubrecording.pl
@@ -1,26 +1,25 @@
-#!/usr/local/bin/perl
-
-# Anne S. Warlaumont
-# Last update: May 18, 2016
-
 # This tool identifies segments within subrecordings of a LENA recording. 
 # A new file containing the start and end times for each segment will be created for each subrecording of the entire recording.
-# e.g., e20131210_144819_009143CHN_AN_Segments1.txt; e20131210_144819_009143CHN_AN_Segments2.txt; e20131210_144819_009143CHN_AN_Segments3.txt
 
 # Take the following command line arguments:
-# 1: The (path and) filename of the pause times file. E.g. "e20131210_144819_009143_PauseTimes.txt"
-# 2: The (path and) filename of the segment times input file. E.g. "e20131210_144819_009143CHN_AN_Segments.txt"
-# e.g. perl segmentsbysubrecording.pl ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN_AN_Segments.txt 
+# 1: The (path and) filename of the pause times file. E.g. "e20080304_150244_003407_PauseTimes.txt"
+# 2: The (path and) filename of the segment times input file. E.g. "e20080304_150244_003407ANStartAndEndTimes.txt" or "e20080304_150244_003407_Segments.csv"
+# 3: The column of the segment times input file that contains the start time. For single speaker files, this will be the first column. For combined speaker files, including those created by segments.pl, this will be the second column.
+# 4: An indication of whether or not there is a header line in the segment times input file. Set to 0 if there's no header. Set to 1 if there is a header.
+# e.g. perl segmentsbysubrecording /PathToPauseTimesFile/e20131126_15502_009145_PauseTimes.txt /PathToOutputOfSegmentsDotPl/e20131126_15502_009145_Segments.txt 2 1
+# e.g. perl segmentsbysubrecording.pl ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145\_PauseTimes.txt ~/Desktop/Gina/Participants/WW04/e20131126\_155022\_009145CHN_AN_Segments.txt 1 0
+# Note some differences between these examples and the screenshots in the documentation
+
+# Written by Anne S. Warlaumont
 
 # For single speaker files, the first column is the start time, but for combined speaker files, the second column is the the start time.
 
-use strict; use warnings;H
+use strict; use warnings;
 
-print "test segmentsbysubrecording input 1: "; print $ARGV[0]; print "\n";
-print "test segmentsbysubrecording input 2: "; print $ARGV[1]; print "\n";
-print "Column containing the start time: "; print $ARGV[2]; print "\n";
+use strict; use warnings;
 
 my $stcol = $ARGV[2];
+my $isHeader = $ARGV[3];
 
 open PAUSEINPUTFILE, $ARGV[0] or die "Could not open the pause times input file\n";
 my @pauses = <PAUSEINPUTFILE>;
@@ -29,17 +28,25 @@ close(PAUSEINPUTFILE);
 open SEGINPUTFILE, $ARGV[1] or die "Could not open the segment times input file\n";
 my $segoutfilebase = $ARGV[1];
 $segoutfilebase =~ s/\.txt//g;
+$segoutfilebase =~ s/\.csv//g;
 my $subreccount = 1;
 open SEGOUTFILE, ">", $segoutfilebase . $subreccount . ".txt" or die "Could not open output file\n";
+if ($isHeader) {
+	my $line = <SEGINPUTFILE>;
+	print SEGOUTFILE $line;
+}
 while (my $line = <SEGINPUTFILE>){
-	#my $line = <SEGINPUTFILE>;
-	my @seginfo = split(m[(?:\s+|^I|\n)+],$line);
+	my @seginfo = split(m[\s|,|\n],$line); # TO do: Add comma separation to this line, and also figure out what the ^I was for when processing one of the old formatted segments files.
 	my $pauseline = $pauses[$subreccount-1];
-	my @pauseinfo = split(m[(?:\s+|^I|\n)+],$pauseline);
+	my @pauseinfo = split(m[\s|,|\n],$pauseline);
 	if ($seginfo[$stcol-1]>$pauseinfo[1]){
 		$subreccount++;
 		close(SEGOUTFILE);
 		open  SEGOUTFILE, ">", $segoutfilebase . $subreccount . ".txt" or die "Could not open output file\n";
+		if ($isHeader) {
+			my $line = <SEGINPUTFILE>;
+			print SEGOUTFILE $line;
+		}
 	}
 	print SEGOUTFILE $line;
 }
